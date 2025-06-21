c716319b2f2248d9ada380ae234c4522922945cb
godbrand <thompsoneregha005@gmail.com>
2025-06-21 17:15:33 +0100

    feat: updated Navbar component to use AuthContext and display user information

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -18,7 +18,7 @@ export const AuthContextProvider = ({ children }) => {
   });
 
   return (
-    <AuthContext.Provider value={{ state, dispatch }}>
+    <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
     </AuthContext.Provider>
   );
